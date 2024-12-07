ec384f38d6d19a1eb32e8c0d8e9e9b45dcecdc5e
anaschwartz <aname4ever@gmail.com>
2024-12-07 01:39:23 -0800

    okay

--- js/sketch.js
+++ js/sketch.js
@@ -4,7 +4,6 @@ document.addEventListener('mousemove', function (e) {
     var offsetY = -60; // Adjust this value to bring the image closer vertically
     
 
-
     handImage.style.left = e.pageX + offsetX + 'px'; // Move horizontally with cursor
     handImage.style.top = e.pageY + offsetY + 'px';  // Move vertically with cursor
     handImage.style.display = 'block';               // Make the image visible as the cursor moves
