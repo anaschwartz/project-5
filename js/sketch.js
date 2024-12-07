f1d23f88b9554142b6f9739464a73b430109ab6a
anaschwartz <aname4ever@gmail.com>
2024-12-07 01:37:04 -0800

    site reupload

---
+++ js/sketch.js
@@ -0,0 +1,12 @@
+document.addEventListener('mousemove', function (e) {
+    var handImage = document.getElementById('hand-follow');
+    var offsetX = -150; // Adjust this value to bring the image closer horizontally
+    var offsetY = -60; // Adjust this value to bring the image closer vertically
+    
+
+
+    handImage.style.left = e.pageX + offsetX + 'px'; // Move horizontally with cursor
+    handImage.style.top = e.pageY + offsetY + 'px';  // Move vertically with cursor
+    handImage.style.display = 'block';               // Make the image visible as the cursor moves
+  });
+
